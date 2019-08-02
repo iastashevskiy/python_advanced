174a5ff7e55f5dafc93813a0fd030dc35393c5e2
Ilya <iastashevskiy@gmail.com>
2019-08-02 13:54:59 +0300

    6

--- server/__main__.py
+++ server/__main__.py
@@ -4,6 +4,7 @@ import yaml
 import json
 from protocol import validate_req, response
 from actions import resolve
+import logging
 
 
 
@@ -24,17 +25,43 @@ if args.config:
 		file_config = yaml.load(file, Loader = yaml.loader)
 		config.update(file_config)
 
+# logger = logging.getLogger('main')
+# formatter = logging.formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# file_handler = logging.FileHandler('main.log')
+# stream_handler = logging.StreamHandler()
+# file_handler.setlevel(logging.DEBUG)
+# stream_handler.setlevel(logging.DEBUG)
+
+# file_handler.setformatter(formatter)
+# stream_handler.setformatter(formatter)
+
+# logger.addHandler(file_handler)
+# logger.addHandler(stream_handler)
+# logger.setlevel(logging.DEBUG)
+
+logging.basicConfig(
+	level = logging.DEBUG
+	format = '%(asctime)s - %(levelname)s - %(message)s'
+	handlers = [
+		logging.FileHandler('main.log'),
+		logging.StreamHandler()
+	]
+
+)
+
 
 host, port = config.get('host'),config.get('port')
+
 try:
 	sock = socket.socket()
 	sock.bind((host, port))
 	sock.listen(5)
-	print(f'Server started with {host}: {port}')
+	logging.info(f'Server started with {host}: {port}')
 
 	while True:
 		client, address = sock.accept()
-		print(f'Client{address[0]}:{address[1]}')
+		logging.info(f'Client{address[0]}:{address[1]}')
 
 		b_request = client.recv(config.get('buffersize'))
 
@@ -47,17 +74,17 @@ try:
 			
 			if controller:	
 				try:
-					print('Request valid', request)
+					logging.info('Request valid', request)
 					response = controller(request)
 				except Exception as error:
-					print('Internal error', error)
+					logging.critical('Internal error', error)
 					response = request(request, 500, 'Internal error')
 			else: 
-				print('Invalid action name: ', action_name)
+				logging.error('Invalid action name: ', action_name)
 				response = response(request, 404,'Action not found')
 
 		else:
-			print('Request invalid', request)
+			logging.error('Request invalid', request)
 			response = response(request, 404, 'Invalid request')
 			
 		str_res = json.dumps(response)
